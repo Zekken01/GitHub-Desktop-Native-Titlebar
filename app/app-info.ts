279841315e6e70e1b8f9401a89edf2dd6a604b07
Zekken <brodycas3@gmail.com>
2026-03-11 22:15:52 -0400

    Disable updates?

--- app/app-info.ts
+++ app/app-info.ts
@@ -1,5 +1,5 @@
 import { getSHA } from './git-info'
-import { getUpdatesURL, getChannel } from '../script/dist-info'
+import { getChannel } from '../script/dist-info'
 import { version, productName } from './package.json'
 
 const devClientId = '3a723b10ac5575cc5bb9'
@@ -25,7 +25,7 @@ export function getReplacements() {
     __DEV__: isDevBuild,
     __DEV_SECRETS__: isDevBuild || !process.env.DESKTOP_OAUTH_CLIENT_SECRET,
     __RELEASE_CHANNEL__: s(channel),
-    __UPDATES_URL__: s(getUpdatesURL()),
+    __UPDATES_URL__: s(''),
     __SHA__: s(getSHA()),
     'process.platform': s(process.platform),
     'process.env.NODE_ENV': s(process.env.NODE_ENV || 'development'),
